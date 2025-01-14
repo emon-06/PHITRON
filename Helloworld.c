e8df33fccb6272318b0df1c155187592ed8e28c0
MD Emon Biswas <148654125+emon-06@users.noreply.github.com>
2025-01-14 15:50:16 +0600

    Update Helloworld.cpp

--- Helloworld.c
+++ Helloworld.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-    printf("Hello World");
+    cout << "Hello World! << endl;
     return 0;
-};
+}
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World");
+//     return 0;
+// };
